4b3fa10cf9de0697f0424df69dc12fc040daed7c
MissJuly <joygitau98@gmail.com>
2023-06-19 15:10:45 +0300

    Incorporate flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/home")
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/shop")
+def shop():
+    return render_template('shop.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
