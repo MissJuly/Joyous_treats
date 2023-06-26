02febbf002d213ebbf863bfd5e6da57e18f4b2ff
MissJuly <joygitau98@gmail.com>
2023-06-26 12:43:22 +0300

    Add search bar

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_login import UserMixin
 
 
 app = Flask(__name__)
+# db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 
 @app.route("/home")
--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,40 @@
-// //adds title overlay effect on images with mouse movement
+//shows search bar when search icon is clicked
+// $(document).readyState(function() {
+//   $('#search-icon').click(function() {
+//     $('#search-overlay').toggleClass('hidden');
+//     $('#search-input').focus();
+//   });
+
+// $('#search-form').submit(function(event) {
+//   event.preventDefault();
+
+//   var searchTerm = $('#search-input').val();
+//   performSearch(searchTerm);
+//   });
+// });
+
+// function performSearch(searchTerm) {
+//   fetch('/search?q=' + searchTerm)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(data) {
+//       displaySearchResults(data);
+//     })
+//     .catch(function(error) {
+//       console.log('An error occurred: ' + error);
+//     });
+// }
+
+
+
+
+
+
+
+
+
+//adds title overlay effect on images with mouse movement
 
 function moveOverlay(event) {
   var container = event.currentTarget;
