e0889cde4496247fbc2eb0c03cd4bdd732b9fda7
MissJuly <joygitau98@gmail.com>
2023-06-27 00:09:45 +0300

    Add the beginnings of flask login

--- static/js/script.js
+++ static/js/script.js
@@ -1,31 +1,42 @@
-//shows search bar when search icon is clicked
-// $(document).readyState(function() {
-//   $('#search-icon').click(function() {
-//     $('#search-overlay').toggleClass('hidden');
-//     $('#search-input').focus();
-//   });
+// shows search bar when search icon is clicked
+$(document).ready(function() {
+  $('#search-icon').click(function() {
+    $('#search-overlay').toggle();
+    $('#search-input').focus();
+  });
 
-// $('#search-form').submit(function(event) {
-//   event.preventDefault();
+$('#search-form').submit(function(event) {
+  event.preventDefault();
 
-//   var searchTerm = $('#search-input').val();
-//   performSearch(searchTerm);
-//   });
-// });
+  var searchTerm = $('#search-input').val();
+  performSearch(searchTerm);
+  });
+});
 
-// function performSearch(searchTerm) {
-//   fetch('/search?q=' + searchTerm)
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(data) {
-//       displaySearchResults(data);
-//     })
-//     .catch(function(error) {
-//       console.log('An error occurred: ' + error);
-//     });
-// }
+function performSearch(searchTerm) {
+  fetch('/search?q=' + searchTerm)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      displaySearchResults(data);
+    })
+    .catch(function(error) {
+      console.log('An error occurred: ' + error);
+    });
+}
 
+function displaySearchResults(results) {
+  var searchResultsDiv = document.getElementById('search-results');
+  searchResultsDiv.innerHTML = '';
+
+  for (var i = 0; i < results.length; i++) {
+    var result = results[i];
+    var resultItem = document.createElement('div');
+    resultItem.textContent = result.title;
+    searchResultsDiv.appendChild(resultItem);
+  }
+}
 
 
 
