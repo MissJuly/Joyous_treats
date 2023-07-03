e966bf0a9f59f84f1412222d69ae87d694b5f07a
MissJuly <joygitau98@gmail.com>
2023-07-03 03:07:26 +0300

    Add neat files

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -8,6 +8,7 @@ from src.accounts.models import User
 from .forms import LoginForm, RegisterForm
 
 
+# Create a Blueprint for the accounts module
 accounts_bp = Blueprint(
     "accounts", __name__,
     template_folder='templates',
@@ -15,16 +16,23 @@ accounts_bp = Blueprint(
     static_url_path="/accounts/static"
 )
 
+# Route for the user registration
 @accounts_bp.route("/register", methods=['GET', 'POST'])
 def register():
+    # Check if the user is already authenticated (logged in)
     if current_user.is_authenticated:
         flash("You are already registered!")
         return redirect(url_for("core.home"))
+
+    # Create a form object from the RegisterForm class
     form = RegisterForm(request.form)
+
+    # Check is the form has been submitted and is valid
     if form.validate_on_submit():
-        # Gets current timestamp
+        # Get current timestamp
         current_time = datetime.now()
 
+        # Create a new User object with the data from the form
         user = User(name=form.name.data,
                     email=form.email.data,
                     phone_number=form.phone_number.data,
@@ -32,35 +40,57 @@ def register():
                     created_at=current_time,
                     updated_at=current_time)
 
+        # Add the new user to the adtabase and commit the changes
         db.session.add(user)
         db.session.commit()
 
+        # Log in the newly registered user
         login_user(user)
         flash("You registered and are now logged in. Welcome!", "success")
 
+        # Redirect the user to the homepage
         return redirect(url_for("core.home"))
 
+    # Render the registration template with the form
     return render_template('register.html', form=form)
 
+# Route for user login
 @accounts_bp.route("/login", methods=['GET', 'POST'])
 def login():
+    # Check if the user is already authenticated (logged in)
     if current_user.is_authenticated:
         flash("You are already logged in.", "info")
         return redirect(url_for("core.home"))
+
+    # Create a form object from the LoginForm class
     form = LoginForm(request.form)
+
+    # Check if the form has been submitted and is valid
     if form.validate_on_submit():
+        # Query the database for the user with the given email
         user = User.query.filter_by(email=form.email.data).first()
+
+        # Check if the user exists and the password is correct
         if user and bcrypt.check_password_hash(user.password, request.form["password"]):
+            # Log in the user
             login_user(user)
+            # Redirect the user to the homepage
             return redirect(url_for("core.home"))
         else:
+            # If the email and/or password is incorrect, show an error message
             flash("Invalid email and/or password.", "danger")
             return render_template('login.html', form=form)
+
+    # Render the login template with the form
     return render_template('login.html', form=form)
 
+# Route for user logout
 @accounts_bp.route("/logout")
 @login_required
 def logout():
+    # Log out the user
     logout_user()
+    # Show a success message
     flash("You were logged out.", "success")
+    # Redirect the user to the login page
     return redirect(url_for("accounts.login"))
--- src/shop/views.py
+++ src/shop/views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 
+
+# Create a Blueprint for the shop section of the website
 shop_bp = Blueprint(
     "shop", __name__,
     template_folder='templates',
@@ -7,22 +9,27 @@ shop_bp = Blueprint(
     static_url_path="/shop/static"
 )
 
+# Define a route for the main shop page
 @shop_bp.route("/shop")
 def shop():
     return render_template('shop.html')
 
+# Define a route for the cakes page
 @shop_bp.route("/shop/cakes")
 def cakes():
     return render_template('cakes.html')
 
+# Define a route for the pies page
 @shop_bp.route("/shop/pies")
 def pies():
     return render_template('pies.html')
 
+# Define a route for the pastries page
 @shop_bp.route("/shop/pastries")
 def pastries():
     return render_template('pastries.html')
 
+# Define a route for the fancy desserts page
 @shop_bp.route("/shop/fancy-desserts")
 def fancy_desserts():
     return render_template('fancy-desserts.html')
